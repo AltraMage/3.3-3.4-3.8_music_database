9d1eace87787736a56953543cb60bdd5af98650d
Logan Simonsen <simonsenl2006@gmail.com>
2024-06-25 09:56:20 +1200

    Uploaded music data, fixed minutes to seconds

---
+++ minutes_to_seconds.py
@@ -0,0 +1,23 @@
+import csv
+
+good_data = []
+
+with open("raw_music_data.csv", "r") as music_data:
+    csv_reader = csv.reader(music_data)
+    # convert string to list 
+    music_list = list(csv_reader)
+
+    for lines in music_list:
+        # 7th column (6th index) is durration
+        try:
+            durration_clean = lines[6].split(":")
+            durration_clean = int(durration_clean[1]) + int(durration_clean[0]) * 60
+            lines[6] = durration_clean
+            good_data.append(lines)
+    
+        except IndexError:
+            ...  # Duration doesnt have an index
+with open('clean_durrations.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(good_data)
+print("Successs, finishing now")
